0687a73efb83673b60a7a4b07c7e5310002eca25
howellva <70650456+howellva@users.noreply.github.com>
2020-10-31 20:36:54 -0400

    Delete api_keys.py

--- python-api-challenge/VacationPy/api_keys.py
+++
@@ -1,7 +0,0 @@
-# OpenWeatherMap API Key
-weather_api_key = "99f4432d572450d7ae0de6c3b259cb1e"
-
-# Google API Key
-#g_key = "AIzaSyBzuKjxTeawHR7TJWCabdhsiOG_VdNmmHE"
-#g_key = "AIzaSyB7O9DAlJIAwRK9sWl4uecGFg7CvHplQPs" 
-g_key = "AIzaSyDkaATsPe8e8yW0ktdBDYJg4BZbdK7Vy84"
